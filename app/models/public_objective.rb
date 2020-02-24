27bb0a1a215317e1a1e200759b2949865e9a0bd6
Dirk Breuer <mail@breuer.io>
2020-02-24 18:26:09 +0100

    Refactored objectives data structure

---
+++ app/models/public_objective.rb
@@ -0,0 +1,12 @@
+class PublicObjective < ValueObject
+  attribute :stage, :string
+  attribute :description, :string
+
+  def self.stage_one
+    all.select { |objective| objective.stage == "one" }
+  end
+
+  def self.stage_two
+    all.select { |objective| objective.stage == "two" }
+  end
+end
