7dbee16ffbebd0e79b5f65f43f1ce3aa0282b4a6
Gregory Igelmund <github.com@grekko.de>
2020-02-25 21:52:01 +0100

    Restore schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -26,16 +26,16 @@ ActiveRecord::Schema.define(version: 2020_02_24_205814) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "winner_id"
-    t.jsonb "player_scores", default: {}
+    t.jsonb "player_scores", default: {}, null: false
     t.integer "custodian_owner_id"
   end
 
   create_table "player_stats", force: :cascade do |t|
     t.string "strategy_card", null: false
-    t.bigint "player_id"
-    t.bigint "round_id"
-    t.jsonb "scored_public_objectives", default: []
-    t.jsonb "scored_secret_objectives", default: []
+    t.bigint "player_id", null: false
+    t.bigint "round_id", null: false
+    t.jsonb "scored_public_objectives", default: [], null: false
+    t.jsonb "scored_secret_objectives", default: [], null: false
     t.index ["player_id"], name: "index_player_stats_on_player_id"
     t.index ["round_id"], name: "index_player_stats_on_round_id"
   end
@@ -44,10 +44,10 @@ ActiveRecord::Schema.define(version: 2020_02_24_205814) do
     t.string "uid", null: false
     t.string "name", null: false
     t.string "faction", null: false
-    t.bigint "game_id"
+    t.bigint "game_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.jsonb "technologies", default: []
+    t.jsonb "technologies", default: [], null: false
     t.integer "seat_number"
     t.index ["game_id"], name: "index_players_on_game_id"
   end
@@ -56,14 +56,17 @@ ActiveRecord::Schema.define(version: 2020_02_24_205814) do
     t.integer "index", default: 1, null: false
     t.datetime "started_at", null: false
     t.datetime "ended_at"
-    t.bigint "game_id"
+    t.bigint "game_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "aasm_state", default: "strategy_phase", null: false
-    t.jsonb "revealed_objectives", default: []
+    t.jsonb "revealed_objectives", default: [], null: false
     t.index ["game_id"], name: "index_rounds_on_game_id"
   end
 
   add_foreign_key "games", "players", column: "winner_id"
+  add_foreign_key "player_stats", "players"
+  add_foreign_key "player_stats", "rounds"
   add_foreign_key "players", "games"
+  add_foreign_key "rounds", "games"
 end
